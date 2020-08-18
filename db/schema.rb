bdf002a3d4261ff72017d35f659a0c4eec12a03c
Raquel Tessarollo <tessarollo.raquel@gmail.com>
2020-08-18 15:23:37 +0200

    Set up a Rails Task Manager

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_094319) do
+ActiveRecord::Schema.define(version: 2020_08_18_094959) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
